592f2fd9ad6e01c08ea9ee99cde8a609b55a6e4b
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2020-08-18 18:34:09 +0200

    Add project files.

---
+++ Resize_Docked_Panel/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Resize_Docked_Panel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_MouseHover(object sender, EventArgs e)
+        {
+
+        }
+
+        Point old_Loc;
+        bool isResizing = false;
+
+
+
+
+        // Mouse Move
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Cursor.Position.X <= this.PointToScreen(panel1.Location).X + 100) // + 100 so there is not unhook when moving the cursor fast
+            {
+
+                // Only Cursor Icon
+                if (Cursor.Position.X <= this.PointToScreen(panel1.Location).X + 10)
+                {
+                    System.Windows.Forms.Cursor.Current = System.Windows.Forms.Cursors.VSplit;
+                }
+
+                if (isResizing == true)
+                {
+                    panel1.Width = panel1.Width - (old_Loc.X + e.X -10);
+                }
+            }
+
+           
+        }
+
+
+
+
+
+        // Mouse Down
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+              
+            if (Cursor.Position.X <= this.PointToScreen(panel1.Location).X + 10)
+            {
+                isResizing = true;
+                System.Windows.Forms.Cursor.Current = System.Windows.Forms.Cursors.VSplit;
+                old_Loc = e.Location;
+            }
+        }
+
+
+
+
+
+        // Mouse UP
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isResizing = false;
+        }
+    }
+}
